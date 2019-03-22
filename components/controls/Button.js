f9c0474970273dfe457ad4dc01ade1624c890e1a
goncy <gonzalo.pozzo4@gmail.com>
2019-03-22 09:06:52 -0300

    Rebuild components

--- components/controls/Button.js
+++ components/controls/Button.js
@@ -1,11 +1,7 @@
 import React from 'react';
 
 var Button = function Button() {
-  return React.createElement(
-    'div',
-    null,
-    '<Button />'
-  );
+  return React.createElement("div", null, "<Button />");
 };
 
 export default Button;
--- components/layout/Screen.js
+++ components/layout/Screen.js
@@ -1,7 +1,14 @@
-var _templateObject = _taggedTemplateLiteral(["\n  margin: 24px;\n  display: flex;\n  flex-direction: column;\n"], ["\n  margin: 24px;\n  display: flex;\n  flex-direction: column;\n"]);
+function _templateObject() {
+  var data = _taggedTemplateLiteral(["\n  margin: 24px;\n  display: flex;\n  flex-direction: column;\n"]);
 
-function _taggedTemplateLiteral(strings, raw) { return Object.freeze(Object.defineProperties(strings, { raw: { value: Object.freeze(raw) } })); }
+  _templateObject = function _templateObject() {
+    return data;
+  };
+
+  return data;
+}
+
+function _taggedTemplateLiteral(strings, raw) { if (!raw) { raw = strings.slice(0); } return Object.freeze(Object.defineProperties(strings, { raw: { value: Object.freeze(raw) } })); }
 
 import styled from "styled-components";
-
-export default styled.main(_templateObject);
+export default styled.main(_templateObject());
